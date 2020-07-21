1cc5b1b92e4cf9f892c66bf64c6526a54a4d838c
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-07-21 13:47:23 +0100

    load refactor

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -26,23 +26,23 @@ class CPU:
     def load(self):
         """Load a program into memory."""
 
-        address = 0
+        if len(sys.argv) != 2:
+            print(f'usage: {sys.argv[0]} filename')
+            sys.exit(1)
 
-        # For now, we've just hardcoded a program:
+        try:
+            with open(sys.argv[1]) as f:
+                for line in f:
+                     num = line.split('#', 1)[0]
+                     if num.strip() == '':
+                         continue
 
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+                    self.ram[address] = int(num, 2)
+                    address += 1
+        
+        except FileNotFoundError:
+            print(f'{sys.argv[0]}: {sys.srgb[1]} not found')
+            sys.exit(2)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
